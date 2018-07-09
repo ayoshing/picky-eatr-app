070d023f27d729abac823bc5d8f93ed1965ae93a
github username <github email address>
2018-07-09 16:40:20 -0400

    first commit

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,36 @@
+class RestaurantsController < ApplicationController
+  def index
+    @restaurants = Restaurant.all
+    if params[:search]
+      @restaurants = Restuarant.select {|restaurant| restaurant.name == params[:search].downcase}
+    else
+      @restaurants = Restaurant.all
+    end
+  end
+
+  def new
+    @powers = Power.all
+    @heroine = Heroine.new
+  end
+
+  def create
+    @heroine = Heroine.new(heroine_params)
+    if @heroine.valid?
+      @heroine.save
+      redirect_to heroine_path(@heroine)
+    else
+      flash[:error] = @heroine.errors.full_messages
+      redirect_to new_heroine_path
+    end
+  end
+
+  def show
+    @heroine = Heroine.find(params[:id])
+  end
+
+  private
+  def heroine_params
+    params.require(:heroine).permit(:name, :super_name, :power_id)
+  end
+
+end
