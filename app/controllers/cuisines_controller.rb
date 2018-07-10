5f24460feda64080fb3d55a3540f9fb4dd441cfb
github username <github email address>
2018-07-10 09:54:06 -0400

    controllers & views implemented

--- app/controllers/cuisines_controller.rb
+++ app/controllers/cuisines_controller.rb
@@ -1,2 +1,20 @@
 class CuisinesController < ApplicationController
+  def index
+    @preferences = Preference.all
+    if params[:search]
+      @preferences = Preference.select {|preference| preference.name == params[:search].downcase}
+    else
+      @preferences = Preference.all
+    end
+  end
+
+  def show
+    @preference = Preference.find(params[:id])
+  end
+
+
+  private
+  def preference_params
+    params.require(:preference).permit(:name)
+  end
 end
--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,2 +1,36 @@
 class FavoritesController < ApplicationController
+  def index
+    @favorites = Favorite.all
+    if params[:search]
+      @favorites = Favorite.select {|favorite| favorite.name == params[:search].downcase}
+    else
+      @favorites = Favorite.all
+    end
+  end
+
+  def show
+    @favorite = Favorite.find(params[:id])
+  end
+
+
+
+  def new
+    @favorite = favorite.new
+  end
+
+  def create
+    @favorite = Favorite.new(favorite_params)
+    if @favorite.valid?
+      @favorite.save
+      redirect_to favorite_path(@favorite)
+    else
+      flash[:error] = @favorite.errors.full_messages
+      redirect_to new_favorite_path
+    end
+  end
+
+  private
+  def favorite_params
+    params.require(:favorite).permit(:user_id, :restaurant_id)
+  end
 end
