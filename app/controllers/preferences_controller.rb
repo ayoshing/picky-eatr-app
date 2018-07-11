e12c4b7accb60be4f934fdef25e0a8c7d550c86f
Shing Wong <shing2487@gmail.com>
2018-07-11 14:29:02 -0400

    able to create cuisine preference instance

--- app/controllers/preferences_controller.rb
+++ app/controllers/preferences_controller.rb
@@ -6,6 +6,9 @@ class PreferencesController < ApplicationController
 
   def show
     @preference = Preference.find(params[:id])
+    @cuisines_preferences = CuisinesPreference.all.select do |cuisine_pref|
+      cuisine_pref.preference_id == @preference.id
+    end
   end
 
   def new
@@ -17,18 +20,18 @@ class PreferencesController < ApplicationController
   def create
     @preference = Preference.new(preference_params)
 
-    # preference_params[:cuisine_ids].each do |id|
-    #   @preference.cuisines << Cuisine.find(id) if id != ""
-    # end
-    byebug
+    preference_params[:cuisine_ids].each do |cuid|
+      CuisinesPreference.create(preference_id: @preference.id, cuisine_id: cuid.to_i)
+    end
+
     @preference.save
-    redirect_to preferences_path
+    redirect_to preference_path(@preference)
 
   end
 
   private
   def preference_params
-    params.require(:preference).permit(:user_id)
+    params.require(:preference).permit(:user_id, cuisine_ids:[])
   end
 
 end
